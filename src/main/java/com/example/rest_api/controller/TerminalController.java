0c252898d1e10f976f8e97fb8ec44ed50648eea7
Willians Carvalho <willians.carvalho81@gmail.com>
2021-08-01 15:12:02 -0300

    Declaring custom exceptions in TerminalController methods

--- src/main/java/com/example/rest_api/controller/TerminalController.java
+++ src/main/java/com/example/rest_api/controller/TerminalController.java
@@ -1,9 +1,13 @@
 package com.example.rest_api.controller;
 
 import com.example.rest_api.dto.TerminalDTO;
+import com.example.rest_api.exception.BadRequestException;
 import com.example.rest_api.exception.CustomNoContentException;
+import com.example.rest_api.exception.CustomNotFoundException;
+import com.example.rest_api.exception.JsonValidationException;
 import com.example.rest_api.model.Terminal;
 import com.example.rest_api.service.TerminalService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,22 +27,22 @@ public class TerminalController {
     }
 
     @GetMapping("/{logic}")
-    public ResponseEntity<Terminal> getTerminal (@PathVariable("logic") int logic) throws Exception {
+    public ResponseEntity<Terminal> getTerminal (@PathVariable("logic") int logic) throws CustomNotFoundException, BadRequestException {
         return terminalService.getTerminal(logic);
     }
 
     @PostMapping
-    public ResponseEntity<Terminal> createTerminal(@RequestBody TerminalDTO terminalDTO) throws Exception {
+    public ResponseEntity<Terminal> createTerminal(@RequestBody TerminalDTO terminalDTO) throws JsonValidationException, JsonProcessingException {
             return terminalService.createTerminal(terminalDTO);
         }
 
     @PutMapping("/{logic}")
-    public ResponseEntity<Terminal> updateTerminal (@PathVariable("logic") int logic, @RequestBody TerminalDTO updateDTO) throws Exception {
+    public ResponseEntity<Terminal> updateTerminal (@PathVariable("logic") int logic, @RequestBody TerminalDTO updateDTO) throws CustomNotFoundException, BadRequestException {
         return terminalService.updateTerminal(logic, updateDTO);
     }
 
     @DeleteMapping("/{logic}")
-    public ResponseEntity<String> deleteTerminal (@PathVariable("logic") int logic) throws Exception {
+    public ResponseEntity<String> deleteTerminal (@PathVariable("logic") int logic) throws CustomNotFoundException, BadRequestException {
        return terminalService.deleteTerminal(logic);
     }
 }
